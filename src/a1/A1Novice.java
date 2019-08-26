4f84f7ee9065388d8b2a181af24edce9d9083c2c
holliscs <holliscs@holliscs-pc.wireless-1x.unc.edu>
2019-08-26 09:28:01 -0400

    First Attempt at A1Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,41 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		// Reading input for the total number of customers
+		int numCust=scan.nextInt();
+		
+		//for loop to iterate through the next inputs based on the number of customers
+		for (int i =0; i<numCust; i++) {
+			//reading in the first name of the customer
+			String fName=scan.next();
+			
+			//reading in last name of the customer
+			String lName=scan.next();
+			
+			//creating a variable for the customer's first initial
+			String fInit=fName.substring(0, 1);
+			
+			//reading in the number of items that the customer bought
+			int numItems=scan.nextInt();
+			
+			//declaring a variable to keep track of the customer's total cost
+			double cTotal=0;
+			//for loop to iterate through next input statements based on how many items the customer bought
+			for (int k=0; k<numItems; k++) {
+				//reading in the quanitity of this item
+				int qItem=scan.nextInt();
+				//reading in the name of the item
+				String iName=scan.next();
+				//reading in item price
+				double iPrice=scan.nextDouble();
+				
+				cTotal+=(iPrice*qItem);
+				
+			}
+			System.out.println(fInit+". "+ lName+": "+cTotal);
+			
+		}
+		
 		
 	}
 }

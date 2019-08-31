11ebd89bce0add123b0c671a133f591103566d59
holliscs <holliscs@holliscs-pc.wireless-1x.unc.edu>
2019-08-31 13:17:56 -0400

    first attempt at A1 jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,7 +8,72 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		//reading in the number of items in the store
+		int iStore=scan.nextInt();
+		
+		//declaring a 2d array to keep track of the items of the store & when they're bought & by how many customers
+		String[][] storeItems = new String[iStore][3];
+		
+		//getting the names and prices of each item in the store
+		for(int i=0; i<iStore; i++) {
+					
+				String iName=scan.next();
+					
+				//we don't care about this, just scanning because the user will input it
+				String iPrice=scan.next();
+					
+				storeItems[i][0]=iName;
+				//starting at 0 because we haven't asked about that yet
+				storeItems[i][1]="0";
+				storeItems[i][2]="0";
+				}
+			
+		//reading in the number of customers
+		int numCust=scan.nextInt();
+		
+		for (int k=0; k<numCust; k++) {
+			// we don't really care about the names just have to read them in 
+			//reading in the first name of the customer
+			String fName=scan.next();
+			
+			//reading in last name of the customer
+			String lName=scan.next();
+			
+			//reading in the number of items that the customer bought
+			int numItems=scan.nextInt();
+			
+			//for loop to iterate through next input statements based on how many items the customer bought
+			for (int j=0; j<numItems; j++) {
+				//reading in the quantity of this item
+				int qItem=scan.nextInt();
+				//reading in the name of the item
+				String iName=scan.next();
+				
+				
+				//finding the cost of the listed item
+				for(int p=0; p< storeItems.length; p++) {
+					int cCount;
+					if(storeItems[p][0].contentEquals(iName)) {
+						//parsing the cost to an integer var so we can do math
+						storeItems[p][1]=""+qItem;
+						cCount=Integer.parseInt(storeItems[p][2]);
+						cCount++;
+						storeItems[p][2]=""+cCount;
+					}
+					
+				}
+					
+			}
+		
+		}
+		for(int d=0; d<storeItems.length; d++) {
+			if(Integer.parseInt(storeItems[d][2])==0) {
+				System.out.println("No customers bought "+storeItems[d][0]);
+			}
+			else {
+				System.out.println(storeItems[d][2]+" customers bought "+ storeItems[d][1]+ " "+ storeItems[d][0]);
+			}
+		}
 		
 	}
 }
